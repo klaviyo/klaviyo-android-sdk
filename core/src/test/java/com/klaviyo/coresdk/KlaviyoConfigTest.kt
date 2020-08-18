d0fccd090e813fe5584ed3b6c394854d55ecba6d
jordan-griffin <jordan.griffin@klaviyo.com>
2020-08-18 14:25:46 -0400

    Add mockito-kotlin for mocking out the context

--- core/src/test/java/com/klaviyo/coresdk/KlaviyoConfigTest.kt
+++ core/src/test/java/com/klaviyo/coresdk/KlaviyoConfigTest.kt
@@ -1,12 +1,17 @@
 package com.klaviyo.coresdk
 
+import android.content.Context
+import com.nhaarman.mockitokotlin2.mock
 import org.junit.Test
 
 class KlaviyoConfigTest {
+    private val contextMock: Context = mock()
+
     @Test
     fun `KlaviyoConfig Builder sets variables successfully`() {
         val config = KlaviyoConfig.Builder()
                 .apiKey("Fake_Key")
+                .applicationContext(contextMock)
                 .networkTimeout(1000)
                 .networkFlushInterval(10000)
                 .build()
@@ -20,6 +25,7 @@ class KlaviyoConfigTest {
     fun `KlaviyoConfig Builder missing variables uses default values successfully`() {
         val config = KlaviyoConfig.Builder()
                 .apiKey("Fake_Key")
+                .applicationContext(contextMock)
                 .build()
 
         assert(config.apiKey == "Fake_Key")
@@ -31,6 +37,7 @@ class KlaviyoConfigTest {
     fun `KlaviyoConfig Builder negative variables uses default values successfully`() {
         val config = KlaviyoConfig.Builder()
                 .apiKey("Fake_Key")
+                .applicationContext(contextMock)
                 .networkTimeout(-5000)
                 .networkFlushInterval(-5000)
                 .build()
@@ -43,6 +50,16 @@ class KlaviyoConfigTest {
     @Test(expected = KlaviyoMissingAPIKeyException::class)
     fun `KlaviyoConfig Builder missing API key throws expected exception`() {
         KlaviyoConfig.Builder()
+                .applicationContext(contextMock)
+                .networkTimeout(500)
+                .networkFlushInterval(60000)
+                .build()
+    }
+
+    @Test(expected = KlaviyoMissingContextException::class)
+    fun `KlaviyoConfig Builder missing application context throws expected exception`() {
+        KlaviyoConfig.Builder()
+                .apiKey("Fake_Key")
                 .networkTimeout(500)
                 .networkFlushInterval(60000)
                 .build()
