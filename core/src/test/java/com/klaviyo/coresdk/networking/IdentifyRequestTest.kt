90b21c5b7a4dac11566b7792d64b6d860ca63545
jordan-griffin <jordan.griffin@klaviyo.com>
2020-08-31 22:35:38 -0400

    Fix test

--- core/src/test/java/com/klaviyo/coresdk/networking/IdentifyRequestTest.kt
+++ core/src/test/java/com/klaviyo/coresdk/networking/IdentifyRequestTest.kt
@@ -22,12 +22,11 @@ class IdentifyRequestTest {
 
     @Test
     fun `Build Identify request successfully`() {
-        val event = "Test Event"
         val properties = hashMapOf("custom_value" to "200")
 
         val expectedJsonString = "{\"event\":\"Test Event\",\"properties\":{\"custom_value\":\"200\"},\"token\":\"Fake_Key\"}"
 
-        val request = IdentifyRequest(event, properties = properties)
+        val request = IdentifyRequest(properties = properties)
         request.queryData = request.buildKlaviyoJsonQuery()
 
         Assert.assertEquals("${KlaviyoRequest.BASE_URL}/${IdentifyRequest.IDENTIFY_ENDPOINT}", request.urlString)
