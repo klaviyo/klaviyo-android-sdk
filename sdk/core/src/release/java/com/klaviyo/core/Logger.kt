aedfec2f0e9661b67c3678d0b5e2cbe58022c22a
Evan Masseau <>
2023-03-01 16:45:41 -0500

    Having these multiple build variant source sets is definitely annoying

--- sdk/core/src/release/java/com/klaviyo/core/Logger.kt
+++ sdk/core/src/release/java/com/klaviyo/core/Logger.kt
@@ -2,6 +2,7 @@ package com.klaviyo.core
 
 import com.klaviyo.core.config.Log
 import com.klaviyo.core.config.NetworkRequest
+import com.klaviyo.core.networking.NetworkRequest
 
 internal class Logger : Log {
     override fun debug(message: String, ex: Exception?) {}
@@ -12,4 +13,5 @@ internal class Logger : Log {
     override fun onLifecycleEvent(event: String) {}
     override fun onNetworkChange(connected: Boolean) {}
     override fun onApiRequest(request: NetworkRequest) {}
+    override fun onDataStore(key: String, value: String?) {}
 }
