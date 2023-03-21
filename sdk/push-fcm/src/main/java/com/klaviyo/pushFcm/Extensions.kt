7c3dc124141e7dda32d52fe7a9893ae488e320dc
Evan Masseau <>
2023-03-21 15:27:45 -0400

    Handle all pushes as data messages. Added utilities for handling push notifications, heavily influenced by FCM sdk code.

---
+++ sdk/push-fcm/src/main/java/com/klaviyo/pushFcm/Extensions.kt
@@ -0,0 +1,213 @@
+package com.klaviyo.pushFcm
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.content.pm.ApplicationInfo
+import android.content.pm.PackageManager
+import android.content.res.Resources
+import android.graphics.drawable.AdaptiveIconDrawable
+import android.net.Uri
+import android.os.Build
+import android.os.Bundle
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import androidx.core.content.PackageManagerCompat
+import androidx.core.content.res.ResourcesCompat
+import com.google.firebase.messaging.CommonNotificationBuilder
+import com.google.firebase.messaging.RemoteMessage
+import com.klaviyo.core.Registry
+
+/**
+ * Parse channel ID or fallback on default
+ */
+val RemoteMessage.channel_id: String
+    get() = this.data[Notification.CHANNEL_ID_KEY] ?: "Default"
+
+/**
+ * Parse channel name or fallback on default
+ */
+val RemoteMessage.channel_name: String
+    get() = this.data[Notification.CHANNEL_NAME_KEY] ?: "Default"
+
+/**
+ * Parse channel description or fallback on default
+ */
+val RemoteMessage.channel_description: String
+    get() = this.data[Notification.CHANNEL_DESCRIPTION_KEY]
+        ?: "Push notifications default channel"
+
+/**
+ * Parse channel importance or fallback on default
+ */
+val RemoteMessage.channel_importance: Int
+    get() = this.data[Notification.CHANNEL_IMPORTANCE_KEY]?.toInt()
+        ?: NotificationManagerCompat.IMPORTANCE_DEFAULT
+
+/**
+ * Parse out notification priority or fallback on default
+ */
+val RemoteMessage.notificationPriority: Int
+    get() = this.data[Notification.NOTIFICATION_PRIORITY]?.toInt()
+        ?: NotificationCompat.PRIORITY_DEFAULT
+
+/**
+ * Determine if the message originated from Klaviyo from the tracking params
+ */
+val RemoteMessage.isKlaviyoMessage: Boolean get() = this.data.containsKey("_k")
+
+/**
+ * Determine if the message is a notification from Klaviyo (as opposed to a silent push)
+ */
+val RemoteMessage.isKlaviyoNotification: Boolean
+    get() = this.isKlaviyoMessage && (title?.let { true } ?: body?.let { true } ?: false)
+
+/**
+ * Parse notification title text
+ */
+val RemoteMessage.title: String? get() = this.data[Notification.TITLE_KEY]
+
+/**
+ * Parse notification body text
+ */
+val RemoteMessage.body: String? get() = this.data[Notification.BODY_KEY]
+
+/**
+ * Parse deep link into a [Uri] if present
+ */
+val RemoteMessage.deepLink: Uri? get() = this.data[Notification.URL_KEY]?.let { Uri.parse(it) }
+
+/**
+ * Parse click action (activity or intent filter)
+ * Click action could be explicitly sent, or we should use ACTION_VIEW if a deep link is sent
+ */
+val RemoteMessage.clickAction: String?
+    get() = this.data[Notification.CLICK_ACTION_KEY] ?: deepLink?.let { Intent.ACTION_VIEW }
+
+/**
+ * Parse [Uri] to sound resource
+ */
+val RemoteMessage.sound: Uri? get() = this.data[Notification.SOUND_KEY]?.let { Uri.parse(it) }
+
+/**
+ * Parse out notification count from payload (for app badging)
+ */
+val RemoteMessage.notificationCount: Int
+    get() = this.data[Notification.NOTIFICATION_COUNT_KEY]?.toInt() ?: 1
+
+/**
+ * Determine the resource ID of the small icon
+ *
+ * NOTE: We have to use a discouraged API because we can't expect
+ *  developers to know the Int value of their icon resources
+ *
+ * @return
+ */
+val RemoteMessage.smallIcon: Int
+    @SuppressLint("DiscouragedApi")
+    get() = this.data[Notification.SMALL_ICON_KEY].let { resourceKey ->
+        val packageManager = Registry.config.applicationContext.packageManager
+        val pkgName = Registry.config.applicationContext.packageName
+        val resources = Registry.config.applicationContext.resources
+
+        /**
+         * API 26 contains a bug that causes the System UI process to crash-loop (which leads to
+         * a factory reset!) if the notification icon is an adaptive icon with a gradient.
+         *
+         * @see [CommonNotificationBuilder.isValidIcon] - FCM method that I am emulating here
+         */
+        fun isValidIcon(resId: Int): Boolean = if (resId == 0) {
+            false
+        } else if (Build.VERSION.SDK_INT != Build.VERSION_CODES.O) {
+            true
+        } else {
+            try {
+                val icon = ResourcesCompat.getDrawable(resources, resId, null)
+                if (icon is AdaptiveIconDrawable) {
+                    Registry.log.error("Adaptive icon $resId is not supported for notification")
+                    false
+                } else {
+                    true
+                }
+            } catch (ex: Resources.NotFoundException) {
+                Registry.log.error("Couldn't find resource $resId for notification")
+                false
+            }
+        }
+
+        if (!resourceKey.isNullOrEmpty()) {
+            var iconId = resources.getIdentifier(resourceKey, "drawable", pkgName)
+            if (isValidIcon(iconId)) {
+                // Drawable icon was found by identifier in resources
+                return iconId
+            }
+
+            iconId = resources.getIdentifier(resourceKey, "mipmap", pkgName)
+            if (isValidIcon(iconId)) {
+                // Mipmap icon was found by identifier in resources
+                return iconId
+            }
+
+            Registry.log.info(
+                "Icon resource $resourceKey not found. Notification will use default icon."
+            )
+        }
+
+        val manifestMetadata = try {
+            val info: ApplicationInfo = packageManager
+                .getApplicationInfoCompat(pkgName, PackageManager.GET_META_DATA)
+
+            info.metaData ?: Bundle.EMPTY
+        } catch (e: PackageManager.NameNotFoundException) {
+            Registry.log.error("Application metadata unavailable", e)
+            Bundle.EMPTY
+        }
+
+        // We allow default icon to be specified in the manifest
+        var iconId = manifestMetadata.getInt(
+            KlaviyoPushService.METADATA_DEFAULT_ICON,
+            // We can also try to get default icon configured for FCM
+            manifestMetadata.getInt(CommonNotificationBuilder.METADATA_DEFAULT_ICON, 0)
+        )
+
+        if (isValidIcon(iconId)) {
+            // Icon found via manifest
+            return iconId
+        }
+
+        try {
+            iconId = packageManager.getApplicationInfoCompat(pkgName).icon
+
+            if (isValidIcon(iconId)) {
+                // Icon found via manifest
+                return iconId
+            }
+        } catch (e: PackageManager.NameNotFoundException) {
+            Registry.log.error("Application info unavailable", e)
+        }
+
+        // Fall back on icon-placeholder used by the OS.
+        return android.R.drawable.sym_def_app_icon
+    }
+
+/**
+ * Extension method since there is no support for this in yet in [PackageManagerCompat]
+ *
+ * NOTE: There is no other option than the deprecated method below Tiramisu
+ *
+ * @param pkgName
+ * @param flags
+ * @return [ApplicationInfo]
+ */
+@Suppress("DEPRECATION")
+private fun PackageManager.getApplicationInfoCompat(
+    pkgName: String,
+    flags: Int = 0
+): ApplicationInfo =
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+        getApplicationInfo(
+            pkgName,
+            PackageManager.ApplicationInfoFlags.of(flags.toLong())
+        )
+    } else {
+        getApplicationInfo(pkgName, flags)
+    }
---
+++ sdk/push-fcm/src/main/java/com/klaviyo/pushFcm/Notification.kt
@@ -0,0 +1,140 @@
+package com.klaviyo.pushFcm
+
+import android.Manifest
+import android.app.Notification
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import androidx.core.app.ActivityCompat
+import androidx.core.app.NotificationChannelCompat
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import com.google.firebase.messaging.RemoteMessage
+import com.klaviyo.core.Registry
+
+class Notification(private val message: RemoteMessage) {
+
+    /**
+     * Constants and extension properties
+     *
+     * NOTE: We always send data-only messages, so all the payload keys are of our choosing.
+     *  For consistency, I've chosen to mostly shadow the FCM notification object keys though.
+     *
+     * See Extensions.kt where accessors are defined as property extensions to [RemoteMessage]
+     */
+    companion object {
+        internal const val CHANNEL_ID_KEY = "channel_id"
+        internal const val CHANNEL_NAME_KEY = "channel_name"
+        internal const val CHANNEL_DESCRIPTION_KEY = "channel_description"
+        internal const val CHANNEL_IMPORTANCE_KEY = "channel_importance"
+        internal const val SMALL_ICON_KEY = "small_icon"
+        internal const val TITLE_KEY = "title"
+        internal const val BODY_KEY = "body"
+        internal const val URL_KEY = "url"
+        internal const val CLICK_ACTION_KEY = "click_action"
+        internal const val SOUND_KEY = "sound"
+        internal const val NOTIFICATION_COUNT_KEY = "notification_count"
+        internal const val NOTIFICATION_PRIORITY = "notification_priority"
+
+        /**
+         * Get an integer ID to associate with a notification or its pending intent
+         * The notification system service will de-dupe on this ID alone,
+         * and I don't think we want our notifications to be de-duped
+         *
+         * NOTE: The FCM SDK also uses a timestamp to construct its integer IDs
+         */
+        private fun generateId() = Registry.clock.currentTimeMillis().toInt()
+    }
+
+    /**
+     * Formats and displays a notification based on the remote message data payload
+     *
+     * @param context
+     */
+    fun displayNotification(context: Context) {
+        if (ActivityCompat.checkSelfPermission(
+                context,
+                Manifest.permission.POST_NOTIFICATIONS
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            return
+        }
+
+        createNotificationChannel(context)
+
+        NotificationManagerCompat
+            .from(context)
+            .notify(
+                generateId(),
+                buildNotification(context)
+            )
+    }
+
+    /**
+     * Creates notification channel if one doesn't exist yet
+     *
+     * NOTE: Uses Compat library for backward compatibility since channels were only added in Oreo
+     */
+    private fun createNotificationChannel(context: Context) {
+        NotificationManagerCompat
+            .from(context)
+            .createNotificationChannel(
+                NotificationChannelCompat.Builder(message.channel_id, message.channel_importance)
+                    .setName(message.channel_name)
+                    .setDescription(message.channel_description)
+                    .build()
+            )
+    }
+
+    /**
+     * Build a [Notification] instance based on the [RemoteMessage] payload
+     *
+     * @param context
+     * @return [Notification] to display
+     */
+    private fun buildNotification(context: Context): Notification =
+        NotificationCompat.Builder(context, message.channel_id)
+            .setContentIntent(createIntent(context))
+            .setSmallIcon(message.smallIcon)
+            .setContentTitle(message.title)
+            .setContentText(message.body)
+            .setSound(message.sound)
+            .setNumber(message.notificationCount)
+            .setPriority(message.notificationPriority)
+            .setAutoCancel(true)
+            .build()
+
+    /**
+     * Create "pending" intent for the notification: essentially a token that
+     * grants another service the ability to invoke a specific intent against our app
+     * This configuration is effectively the same as how FCM notification builder does it.
+     * https://developer.android.com/reference/android/app/PendingIntent
+     *
+     * It contains the action intent that will be invoked when the notification is clicked
+     * This builder logic mimics the behavior from the FCM SDK notification handler
+     *
+     * @return [PendingIntent]
+     */
+    private fun createIntent(context: Context): PendingIntent {
+        val pkgName = Registry.config.applicationContext.packageName
+
+        // Create intent to open the activity and/or deep link if specified
+        // Else fall back on the default launcher intent for the package
+        val action = message.clickAction?.let {
+            message.toIntent().apply {
+                action = message.clickAction
+                data = message.deepLink
+                setPackage(pkgName)
+                addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP)
+            }
+        } ?: Registry.config.applicationContext.packageManager.getLaunchIntentForPackage(pkgName)
+
+        return PendingIntent.getActivity(
+            context,
+            generateId(),
+            action,
+            PendingIntent.FLAG_IMMUTABLE or PendingIntent.FLAG_ONE_SHOT
+        )
+    }
+}
