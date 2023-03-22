ef928f05fc58e09a236719b1fc738e7330d9c17f
Evan C Masseau <5167687+evan-masseau@users.noreply.github.com>
2023-03-22 11:23:46 -0400

    Add full remote message handling to SDK (#56)
    * Handle all pushes as data messages. Added utilities for handling push notifications
    * Defined accessors to our remote message data as extension functions in an object so they aren't automatically exposed to our developers -- at least they have to import the methods.
    * README updates related to these data-message changes, icons, advanced instructions
    
    ---------
    
    Co-authored-by: Evan Masseau <>

--- sdk/push-fcm/src/main/java/com/klaviyo/pushFcm/KlaviyoPushService.kt
+++ sdk/push-fcm/src/main/java/com/klaviyo/pushFcm/KlaviyoPushService.kt
@@ -3,6 +3,7 @@ package com.klaviyo.pushFcm
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 import com.klaviyo.analytics.Klaviyo
+import com.klaviyo.pushFcm.KlaviyoRemoteMessage.isKlaviyoNotification
 
 /**
  * Implementation of the FCM messaging service that runs when the parent application is started
@@ -11,15 +12,16 @@ import com.klaviyo.analytics.Klaviyo
  * If the parent application has their own FCM messaging service defined they need to ensure
  * that the implementation details of this service are carried over into their own
  */
-class KlaviyoPushService : FirebaseMessagingService() {
+open class KlaviyoPushService : FirebaseMessagingService() {
+
+    companion object {
+        const val METADATA_DEFAULT_ICON = "com.klaviyo.push.default_notification_icon"
+    }
 
     /**
-     * FCM service calls this function whenever a token is generated
-     * This can be whenever a token is created anew, or whenever it has expired and regenerated itself
+     * Called when FCM SDK receives a newly registered token
      *
-     * Invoke the SDK to log the push token to the profile
-     *
-     * @param newToken The newly generated token returned from the FCM service
+     * @param newToken
      */
     override fun onNewToken(newToken: String) {
         super.onNewToken(newToken)
@@ -27,14 +29,20 @@ class KlaviyoPushService : FirebaseMessagingService() {
     }
 
     /**
-     * FCM calls this when any remote message is received
-     * while the app is in the foreground, or if a data message is received
-     * while the app is backgrounded.
+     * This method is invoked from FCM SDK
+     *  when a "notification" message is received while the app is in the foreground,
+     *  and when a "data" message is received regardless of the app's status
+     *
+     * Klaviyo message payload is always formatted as a "data" message to retain
+     * full control over display logic, so all Klaviyo push messages come through this method.
      *
      * @param message Remote message that has been received
      */
     override fun onMessageReceived(message: RemoteMessage) {
-        // TODO could we provide a way to display the notification in system tray?
         super.onMessageReceived(message)
+
+        if (message.isKlaviyoNotification) {
+            KlaviyoNotification(message).displayNotification(this)
+        }
     }
 }
