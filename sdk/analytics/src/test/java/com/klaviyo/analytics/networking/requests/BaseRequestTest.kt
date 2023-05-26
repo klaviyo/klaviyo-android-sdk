9537a4e71c4d27aae4a4662fbcbdc87e1bf2b681
Kenny Tsui <kenny.tsui@klaviyo.com>
2023-05-26 16:17:03 -0400

    mock all fields

--- sdk/analytics/src/test/java/com/klaviyo/analytics/networking/requests/BaseRequestTest.kt
+++ sdk/analytics/src/test/java/com/klaviyo/analytics/networking/requests/BaseRequestTest.kt
@@ -13,5 +13,14 @@ internal open class BaseRequestTest : BaseTest() {
         super.setup()
         mockkObject(DeviceProperties)
         every { DeviceProperties.userAgent } returns "Mock User Agent"
+        every { DeviceProperties.model } returns "Mock Model"
+        every { DeviceProperties.applicationLabel } returns "Mock Application Label"
+        every { DeviceProperties.appVersion } returns "Mock App Version"
+        every { DeviceProperties.appVersionCode } returns "Mock Version Code"
+        every { DeviceProperties.sdkVersion } returns "Mock SDK Version"
+        every { DeviceProperties.applicationId } returns "Mock Application Id"
+        every { DeviceProperties.platform } returns "Mock Platform"
+        every { DeviceProperties.manufacturer } returns "Mock Manufacturer"
+        every { DeviceProperties.osVersion } returns "Mock OS Version"
     }
 }
