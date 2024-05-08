3a05e1d18b54cf2175f2ef4588f1562583c0a51a
Evan Masseau <>
2024-05-08 15:04:24 -0400

    Rename these properties so it is clear what true and false mean. Fix polarity of how we check background data

--- sdk/analytics/src/test/java/com/klaviyo/analytics/KlaviyoTest.kt
+++ sdk/analytics/src/test/java/com/klaviyo/analytics/KlaviyoTest.kt
@@ -401,7 +401,7 @@ internal class KlaviyoTest : BaseTest() {
 
     @Test
     fun `Push token request is repeated if state has changed`() {
-        every { DeviceProperties.backgroundData } returns true
+        every { DeviceProperties.backgroundDataEnabled } returns true
         Klaviyo.setPushToken(PUSH_TOKEN)
         assertEquals(PUSH_TOKEN, dataStoreSpy.fetch("push_token"))
 
@@ -409,7 +409,7 @@ internal class KlaviyoTest : BaseTest() {
             apiClientMock.enqueuePushToken(PUSH_TOKEN, any())
         }
 
-        every { DeviceProperties.backgroundData } returns false
+        every { DeviceProperties.backgroundDataEnabled } returns false
         Klaviyo.setPushToken(PUSH_TOKEN)
 
         verify(exactly = 2) {
