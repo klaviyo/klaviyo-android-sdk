08590d8427262efd6ba9961c405469fcfd7f3b6e
Evan C Masseau <5167687+evan-masseau@users.noreply.github.com>
2024-05-23 09:58:57 -0400

    API Header Fix: Move attempt count increment to before we write request headers into the url connection. (#170)
    Co-authored-by: Evan Masseau <>

--- sdk/analytics/src/main/java/com/klaviyo/analytics/networking/requests/KlaviyoApiRequest.kt
+++ sdk/analytics/src/main/java/com/klaviyo/analytics/networking/requests/KlaviyoApiRequest.kt
@@ -283,12 +283,12 @@ internal open class KlaviyoApiRequest(
         }
 
         status = Status.Inflight
+        attempts++
 
         return try {
             val connection = buildUrlConnection()
 
             try {
-                attempts++
                 beforeSend.invoke()
                 connection.connect()
                 parseResponse(connection)
