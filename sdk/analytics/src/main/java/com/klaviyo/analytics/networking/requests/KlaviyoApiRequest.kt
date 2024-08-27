bb7b49d1ef5c52225ff72bfc9f3c41ed4359afb2
Daniel Peluso <daniel.peluso@klaviyo.com>
2024-08-27 13:22:43 -0400

    fixing http import

--- sdk/analytics/src/main/java/com/klaviyo/analytics/networking/requests/KlaviyoApiRequest.kt
+++ sdk/analytics/src/main/java/com/klaviyo/analytics/networking/requests/KlaviyoApiRequest.kt
@@ -6,7 +6,6 @@ import java.io.BufferedReader
 import java.io.IOException
 import java.io.InputStreamReader
 import java.net.HttpURLConnection
-import java.net.HttpURLConnection.HTTP_UNAVAILABLE
 import java.net.URL
 import java.util.UUID
 import javax.net.ssl.HttpsURLConnection
