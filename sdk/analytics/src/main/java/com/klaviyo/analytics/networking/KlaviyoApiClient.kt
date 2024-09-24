760c6695e56db4d2c28f3cf6bea51c61cac6d21e
Evan Masseau <>
2024-09-24 11:36:59 -0400

    Cherry pick Dan's bug fix for concurrency problem

--- sdk/analytics/src/main/java/com/klaviyo/analytics/networking/KlaviyoApiClient.kt
+++ sdk/analytics/src/main/java/com/klaviyo/analytics/networking/KlaviyoApiClient.kt
@@ -14,6 +14,7 @@ import com.klaviyo.analytics.networking.requests.ProfileApiRequest
 import com.klaviyo.analytics.networking.requests.PushTokenApiRequest
 import com.klaviyo.core.Registry
 import com.klaviyo.core.lifecycle.ActivityEvent
+import java.util.Collections
 import java.util.concurrent.ConcurrentLinkedDeque
 import org.json.JSONArray
 import org.json.JSONException
@@ -33,7 +34,9 @@ internal object KlaviyoApiClient : ApiClient {
     /**
      * List of registered API observers
      */
-    private var apiObservers = mutableListOf<ApiObserver>()
+    private val apiObservers = Collections.synchronizedList(
+        mutableListOf<ApiObserver>()
+    )
 
     /**
      * Initialize logic including lifecycle observers and reviving the queue from persistent store
@@ -136,7 +139,9 @@ internal object KlaviyoApiClient : ApiClient {
             Registry.log.verbose("${request.responseCode} $response")
         }
 
-        apiObservers.forEach { it(request) }
+        synchronized(apiObservers) {
+            apiObservers.forEach { it(request) }
+        }
     }
 
     /**
