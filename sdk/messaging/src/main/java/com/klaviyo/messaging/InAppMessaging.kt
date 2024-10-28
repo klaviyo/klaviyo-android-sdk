98542089340d22310d12afa09282f19040171679
Dan Peluso <daniel.peluso@klaviyo.com>
2024-10-28 15:58:09 -0400

    CHNL-13253 IAM Basic Webview + Module Scaffolding (#194)
    * Initial messaging module with webview dependency
    
    * Revert "Remove sample app from the repo, we'll just revert this commit to continue work on it later. (#46)"
    
    This reverts commit da1be69d93a911baf760043fa7e474f9de6f2225.
    
    * catch sample app up
    
    * Migrate sample to refreshVersions
    
    * Simple update to the sample code
    
    * Basic ability to add a webview to a provided Activity
    
    * Can't use this till google services file is added!
    
    * Implemented javascript bridge using newer android webview APIs and fallback older methods.
    Added bridge JS file that facilitates communication between webview/native, including detecting when all image tags have finished loading.
    
    * readability
    
    * commenting and cleanup
    
    * Native close button
    
    * Buncha progress on sample app
    
    * Add an actual view for interactivity
    
    * Inject Klaviyo Forms and Hook into Requests
    
    * making webview visible
    
    * deleting sample app (adding to test app)
    
    * pr comments
    
    * trying to remove code I think was only relevant for sample app
    
    * remove user agent string from webview
    
    * making user agent property public in DeviceProperties
    
    ---------
    
    Co-authored-by: Evan Masseau <>
    Co-authored-by: kennyklaviyo <kenny.tsui@klaviyo.com>

---
+++ sdk/messaging/src/main/java/com/klaviyo/messaging/InAppMessaging.kt
@@ -0,0 +1,26 @@
+package com.klaviyo.messaging
+
+import android.app.Activity
+import android.view.ViewGroup
+import com.klaviyo.core.Registry
+import java.io.BufferedReader
+
+object InAppMessaging {
+
+    fun triggerInAppMessage(activity: Activity) {
+        val rootView = activity.getRootViewGroup()
+        val webView = KlaviyoWebView()
+        val html = Registry.config.applicationContext
+            .assets
+            .open("IAMTest.html")
+            .bufferedReader()
+            .use(BufferedReader::readText)
+            .replace("KLAVIYO_PUBLIC_KEY_PLACEHOLDER", Registry.config.apiKey)
+
+        webView.loadHtml(html)
+        webView.addTo(rootView)
+    }
+
+    private fun Activity.getRootViewGroup(): ViewGroup =
+        window.decorView.findViewById(android.R.id.content)
+}
