76501efbb6800c238cc2f5a2c83a22982c9d7afd
Dan Peluso <daniel.peluso@klaviyo.com>
2025-01-28 15:28:20 -0500

    [CHNL-16653] Enqueue Profile Events (#209)
    * [CHNL-16653] Enqueue Profile Events
    
    * add exception to the mapper function run
    
    * pr comments

---
+++ sdk/messaging/src/main/java/com/klaviyo/messaging/KlaviyoWebFormMessageType.kt
@@ -0,0 +1,14 @@
+package com.klaviyo.messaging
+
+import com.klaviyo.analytics.model.Event
+
+/**
+ * This should be updated with any new message types we add coming from the onsite-in-app-forms
+ */
+sealed class KlaviyoWebFormMessageType {
+    data object Show : KlaviyoWebFormMessageType()
+    data object Close : KlaviyoWebFormMessageType()
+    data class ProfileEvent(
+        val event: Event
+    ) : KlaviyoWebFormMessageType()
+}
