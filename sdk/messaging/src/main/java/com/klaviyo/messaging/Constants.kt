98ce4d63aa7205ec7d2e1ad6ee9d80676c7375b3
Dan Peluso <daniel.peluso@klaviyo.com>
2025-01-28 16:38:55 -0500

    [CHNL-16704] Deeplink Support for IAF (#213)
    * support mapping and unit test
    
    * supporting deeplinks and weak activity ref in the webview
    
    * pr comments

--- sdk/messaging/src/main/java/com/klaviyo/messaging/Constants.kt
+++ sdk/messaging/src/main/java/com/klaviyo/messaging/Constants.kt
@@ -1,6 +1,13 @@
 package com.klaviyo.messaging
 
 /**
+ * Fields to replace in the HTML template
+ */
+internal const val IAF_SDK_NAME_PLACEHOLDER = "SDK_NAME"
+internal const val IAF_SDK_VERSION_PLACEHOLDER = "SDK_VERSION"
+internal const val IAF_PUBLIC_KEY_PLACEHOLDER = "KLAVIYO_PUBLIC_KEY_PLACEHOLDER"
+
+/**
  * Decoding top-level message types
  */
 internal const val IAF_MESSAGE_DATA_KEY = "data"
@@ -9,9 +16,15 @@ internal const val IAF_MESSAGE_TYPE_SHOW = "formDidAppear"
 internal const val IAF_MESSAGE_TYPE_CLOSE = "formDidClose"
 internal const val IAF_MESSAGE_TYPE_PROFILE_EVENT = "profileEventTracked"
 internal const val IAF_MESSAGE_TYPE_AGGREGATE_EVENT = "aggregateEventTracked"
+internal const val IAF_MESSAGE_TYPE_DEEPLINK = "deepLinkShouldOpen"
 
 /**
  * Profile event constants
  */
 internal const val IAF_METRIC_KEY = "metric"
 internal const val IAF_PROPERTIES_KEY = "properties"
+
+/**
+ * Deep Link fields
+ */
+internal const val IAF_DEEPLINK_ANDROID = "android"
