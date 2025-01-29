50e041f63276b9d4d1d65a37bf3ed354c366140d
Daniel Peluso <daniel.peluso@klaviyo.com>
2025-01-29 17:09:26 -0500

    proposed iaf api

--- sdk/messaging/src/main/java/com/klaviyo/messaging/InAppMessaging.kt
+++ sdk/messaging/src/main/java/com/klaviyo/messaging/InAppMessaging.kt
@@ -1,28 +1,55 @@
 package com.klaviyo.messaging
 
 import android.app.Activity
+import android.app.Application
+import android.os.Bundle
 import android.view.ViewGroup
 import com.klaviyo.core.Registry
 import java.io.BufferedReader
+import java.lang.ref.WeakReference
 
 object InAppMessaging {
 
-    fun triggerInAppMessage(activity: Activity) {
-        val rootView = activity.getRootViewGroup()
-        val webView = KlaviyoWebView(activity)
-        val html = activity
-            .assets
-            .open("IAMTest.html")
-            .bufferedReader()
-            .use(BufferedReader::readText)
-            .replace(IAF_PUBLIC_KEY_PLACEHOLDER, Registry.config.apiKey)
-            .replace(IAF_SDK_NAME_PLACEHOLDER, Registry.config.sdkName)
-            .replace(IAF_SDK_VERSION_PLACEHOLDER, Registry.config.sdkVersion)
+    private var currentActivityReference: WeakReference<Activity>? = null
 
-        webView.loadHtml(html)
-        webView.addTo(rootView)
+    fun triggerInAppMessage() {
+        getCurrentActivity()?.let { activity ->
+            val rootView = activity.getRootViewGroup()
+            val webView = KlaviyoWebView(activity)
+            val html = activity
+                .assets
+                .open("IAMTest.html")
+                .bufferedReader()
+                .use(BufferedReader::readText)
+                .replace(IAF_PUBLIC_KEY_PLACEHOLDER, Registry.config.apiKey)
+                .replace(IAF_SDK_NAME_PLACEHOLDER, Registry.config.sdkName)
+                .replace(IAF_SDK_VERSION_PLACEHOLDER, Registry.config.sdkVersion)
+
+            webView.loadHtml(html)
+            webView.addTo(rootView)
+        }
     }
 
     private fun Activity.getRootViewGroup(): ViewGroup =
         window.decorView.findViewById(android.R.id.content)
+
+    fun registerForInAppMessages(application: Application) {
+        application.registerActivityLifecycleCallbacks(object : Application.ActivityLifecycleCallbacks {
+            override fun onActivityCreated(activity: Activity, savedInstanceState: Bundle?) {}
+            override fun onActivityStarted(activity: Activity) {}
+            override fun onActivityResumed(activity: Activity) {
+                currentActivityReference = WeakReference(activity)
+            }
+            override fun onActivityPaused(activity: Activity) {}
+            override fun onActivityStopped(activity: Activity) {}
+            override fun onActivitySaveInstanceState(activity: Activity, outState: Bundle) {}
+            override fun onActivityDestroyed(activity: Activity) {}
+        })
+        // todo this would be a good idea to add when we want to 'stop listening'. probably will attach to application ondestroy
+        // application.unregisterActivityLifecycleCallbacks()
+    }
+
+    internal fun getCurrentActivity(): Activity? {
+        return currentActivityReference?.get()
+    }
 }
