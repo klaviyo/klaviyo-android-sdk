947e5a99721e6370526635d03422b54393d51f36
Evan C Masseau <5167687+evan-masseau@users.noreply.github.com>
2025-02-10 12:19:58 -0500

    IAF handshake (#216)
    * Inject native bridge handshake data
    
    * Use string substitution for klaviyo.js script tag. Move base URL of klaviyo.js to a config (paves the way for test app to be able to set it)
    
    * Implement a basic abort event whynot
    
    * Not a test anymore :D
    
    ---------
    
    Co-authored-by: Evan Masseau <>

--- sdk/messaging/src/main/java/com/klaviyo/messaging/InAppMessaging.kt
+++ sdk/messaging/src/main/java/com/klaviyo/messaging/InAppMessaging.kt
@@ -10,14 +10,17 @@ object InAppMessaging {
     fun triggerInAppMessage(activity: Activity) {
         val rootView = activity.getRootViewGroup()
         val webView = KlaviyoWebView(activity)
+        val klaviyoJsUrl = "${Registry.config.baseCdnUrl}/onsite/js/klaviyo.js?env=in-app&company_id=${Registry.config.apiKey}"
         val html = activity
             .assets
-            .open("IAMTest.html")
+            .open("InAppFormsTemplate.html")
             .bufferedReader()
             .use(BufferedReader::readText)
-            .replace(IAF_PUBLIC_KEY_PLACEHOLDER, Registry.config.apiKey)
+            .replace(IAF_BRIDGE_NAME_PLACEHOLDER, IAF_BRIDGE_NAME)
             .replace(IAF_SDK_NAME_PLACEHOLDER, Registry.config.sdkName)
             .replace(IAF_SDK_VERSION_PLACEHOLDER, Registry.config.sdkVersion)
+            .replace(IAF_HANDSHAKE_PLACEHOLDER, IAF_HANDSHAKE)
+            .replace(IAF_KLAVIYO_JS_PLACEHOLDER, klaviyoJsUrl)
 
         webView.loadHtml(html)
         webView.addTo(rootView)
