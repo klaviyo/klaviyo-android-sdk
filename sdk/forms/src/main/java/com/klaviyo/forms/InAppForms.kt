0e40bf29cd650c9915b7defa869fcedfaf10615c
Evan Masseau <>
2025-02-25 10:06:06 -0500

    nvm on using an object

--- sdk/forms/src/main/java/com/klaviyo/forms/InAppForms.kt
+++ sdk/forms/src/main/java/com/klaviyo/forms/InAppForms.kt
@@ -4,16 +4,13 @@ import com.klaviyo.analytics.Klaviyo
 import com.klaviyo.core.Registry
 import com.klaviyo.core.safeApply
 
-object KlaviyoForms {
-    private fun initialize() {
-        if (!Registry.isRegistered<KlaviyoWebViewDelegate>()) {
-            Registry.register<KlaviyoWebViewDelegate>(KlaviyoWebViewDelegate())
-        }
+/**
+ * Register for in-app forms triggers
+ */
+fun Klaviyo.registerForInAppForms(): Klaviyo = safeApply {
+    if (!Registry.isRegistered<KlaviyoWebViewDelegate>()) {
+        Registry.register<KlaviyoWebViewDelegate>(KlaviyoWebViewDelegate())
     }
 
-    fun Klaviyo.registerForInAppForms(): Klaviyo = safeApply {
-        initialize()
-
-        Registry.get<KlaviyoWebViewDelegate>().initializeWebView()
-    }
+    Registry.get<KlaviyoWebViewDelegate>().initializeWebView()
 }
