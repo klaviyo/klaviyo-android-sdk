26dbb751499d96ba287f81d2b88a374df187b7d0
Daniel Peluso <daniel.peluso@klaviyo.com>
2025-05-07 11:19:34 -0400

    [CHNL-19776] In App Forms Config

---
+++ sdk/forms/src/main/java/com/klaviyo/forms/InAppFormsConfig.kt
@@ -0,0 +1,10 @@
+package com.klaviyo.forms
+
+/**
+ * Configuration for in app forms
+ *
+ * @param sessionTimeoutDuration (seconds) timeout for listening to new in app forms to display
+ */
+data class InAppFormsConfig(
+    val sessionTimeoutDuration: Long = 3600
+)
