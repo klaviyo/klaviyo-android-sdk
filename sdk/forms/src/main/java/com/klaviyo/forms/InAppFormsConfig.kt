1a8f736ae9cbf3d6698af3d8f282b1ba46427fdf
Daniel Peluso <daniel.peluso@klaviyo.com>
2025-05-12 11:56:04 -0400

    adding const for default timeout, updating param desc

--- sdk/forms/src/main/java/com/klaviyo/forms/InAppFormsConfig.kt
+++ sdk/forms/src/main/java/com/klaviyo/forms/InAppFormsConfig.kt
@@ -3,8 +3,15 @@ package com.klaviyo.forms
 /**
  * Configuration for in app forms
  *
- * @param sessionTimeoutDuration (seconds) timeout for listening to new in app forms to display
+ * @param sessionTimeoutDuration Duration (in seconds) of the period of user inactivity after which the user's app session is terminated. Defaults to 1 Hour.
  */
 data class InAppFormsConfig(
-    val sessionTimeoutDuration: Long = 3600
-)
+    val sessionTimeoutDuration: Long = DEFAULT_SESSION_TIMEOUT
+) {
+    companion object {
+        /***
+         * One hour - meaning we will destroy the forms connection after an hour of inactivity
+         */
+        const val DEFAULT_SESSION_TIMEOUT = 3600L
+    }
+}
