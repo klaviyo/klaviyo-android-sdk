3a4095fcc4dd95bdc3680a7488e851c9d3d61a99
Evan C Masseau <5167687+evan-masseau@users.noreply.github.com>
2025-05-13 16:27:24 -0400

    IAF | Presentation Manager & Extracted Interfaces (#261)
    Split out a presentation manager service and lift presentation/initialization logic there
    Extract interfaces and organize into subpackages similar to our other SDK modules.
    Lays ground work for migrating to an overlay activity
    
    ---------
    
    Co-authored-by: Evan Masseau <>

--- sdk/forms/src/test/java/com/klaviyo/forms/InAppFormsTest.kt
+++ sdk/forms/src/test/java/com/klaviyo/forms/InAppFormsTest.kt
@@ -4,11 +4,18 @@ import com.klaviyo.analytics.Klaviyo
 import com.klaviyo.core.MissingConfig
 import com.klaviyo.core.Registry
 import com.klaviyo.fixtures.BaseTest
+import com.klaviyo.forms.bridge.BridgeMessageHandler
+import com.klaviyo.forms.bridge.KlaviyoBridgeMessageHandler
+import com.klaviyo.forms.presentation.KlaviyoPresentationManager
+import com.klaviyo.forms.presentation.PresentationManager
+import com.klaviyo.forms.webview.KlaviyoWebViewClient
+import com.klaviyo.forms.webview.WebViewClient
 import io.mockk.every
 import io.mockk.mockk
 import io.mockk.mockkConstructor
 import io.mockk.verify
 import org.junit.After
+import org.junit.Assert.assertEquals
 import org.junit.Before
 import org.junit.Test
 
@@ -17,30 +24,52 @@ internal class InAppFormsTest : BaseTest() {
     @Before
     override fun setup() {
         super.setup()
-        mockkConstructor(KlaviyoWebViewClient::class)
+        mockkConstructor(KlaviyoPresentationManager::class)
+        mockkConstructor(KlaviyoBridgeMessageHandler::class)
+        mockkConstructor(KlaviyoWebViewClient::class).apply {
+            every { anyConstructed<KlaviyoWebViewClient>().initializeWebView() } returns this
+        }
     }
 
     @After
     override fun cleanup() {
-        Registry.unregister<KlaviyoWebViewClient>()
+        Registry.unregister<PresentationManager>()
+        Registry.unregister<BridgeMessageHandler>()
+        Registry.unregister<WebViewClient>()
         super.cleanup()
     }
 
     @Test
-    fun `initializes with pre-registered delegate`() {
-        val delegate: KlaviyoWebViewClient = mockk(relaxed = true)
-        Registry.register<KlaviyoWebViewClient>(delegate)
+    fun `registers required services`() {
+        assert(!Registry.isRegistered<PresentationManager>())
+        assert(!Registry.isRegistered<BridgeMessageHandler>())
+        assert(!Registry.isRegistered<WebViewClient>())
+
         Klaviyo.registerForInAppForms()
 
-        verify { delegate.initializeWebView() }
+        assert(Registry.isRegistered<PresentationManager>())
+        assert(Registry.isRegistered<BridgeMessageHandler>())
+        assert(Registry.isRegistered<WebViewClient>())
     }
 
     @Test
-    fun `registers a delegate if we don't have one`() {
-        every { anyConstructed<KlaviyoWebViewClient>().initializeWebView() } returns mockk()
-        assert(!Registry.isRegistered<KlaviyoWebViewClient>())
+    fun `registers required services once`() {
+        val presenter: PresentationManager = mockk()
+        val bridge: BridgeMessageHandler = mockk()
+        val client: WebViewClient = mockk<WebViewClient>().apply {
+            every { initializeWebView() } returns Unit
+        }
+        Registry.register<PresentationManager>(presenter)
+        Registry.register<BridgeMessageHandler>(bridge)
+        Registry.register<WebViewClient>(client)
+
         Klaviyo.registerForInAppForms()
-        assert(Registry.isRegistered<KlaviyoWebViewClient>())
+
+        // It used our pre-registered services, and didn't overwrite them
+        assertEquals(presenter, Registry.get<PresentationManager>())
+        assertEquals(bridge, Registry.get<BridgeMessageHandler>())
+        assertEquals(client, Registry.get<WebViewClient>())
+        verify { client.initializeWebView() }
     }
 
     @Test(expected = Test.None::class)
