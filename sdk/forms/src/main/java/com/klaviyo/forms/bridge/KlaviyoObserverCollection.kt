3050e4379208d1e3a5edfe17c429c57bee71b55e
Evan Masseau <>
2025-05-19 15:06:20 -0400

    Add profile observer implementation

--- sdk/forms/src/main/java/com/klaviyo/forms/bridge/KlaviyoObserverCollection.kt
+++ sdk/forms/src/main/java/com/klaviyo/forms/bridge/KlaviyoObserverCollection.kt
@@ -5,6 +5,7 @@ package com.klaviyo.forms.bridge
  */
 internal class KlaviyoObserverCollection : ObserverCollection {
     override val observers: List<Observer> = listOf(
-        LifecycleObserver()
+        LifecycleObserver(),
+        ProfileObserver(),
     )
 }
