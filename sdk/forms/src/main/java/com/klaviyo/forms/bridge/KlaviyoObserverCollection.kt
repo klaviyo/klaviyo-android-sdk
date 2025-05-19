f238d86fb9184b9c4489fe0f92f72ade964b01c3
Evan Masseau <>
2025-05-19 15:05:49 -0400

    Strip out observer implementations, to make this a pure interface/architectural branch.

--- sdk/forms/src/main/java/com/klaviyo/forms/bridge/KlaviyoObserverCollection.kt
+++ sdk/forms/src/main/java/com/klaviyo/forms/bridge/KlaviyoObserverCollection.kt
@@ -1,7 +1,12 @@
 package com.klaviyo.forms.bridge
 
+/**
+ * TODO Manage all observers that inject data into the webview, to include
+ *  - Profile identifiers
+ *  - Lifecycle events
+ *  - Analytics events
+ *  - Host App navigation events
+ */
 internal class KlaviyoObserverCollection : ObserverCollection {
-    override val observers: List<Observer> = listOf(
-        ProfileObserver()
-    )
+    override val observers: List<Observer> = emptyList()
 }
