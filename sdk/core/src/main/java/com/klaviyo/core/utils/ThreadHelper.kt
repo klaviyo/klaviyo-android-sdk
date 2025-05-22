3c9f5bc96f5cf1e099eec8442c5f181acbaa3816
Evan Masseau <>
2025-05-22 18:58:07 -0400

    Move thread util to core package so it can be shared

---
+++ sdk/core/src/main/java/com/klaviyo/core/utils/ThreadHelper.kt
@@ -0,0 +1,14 @@
+package com.klaviyo.core.utils
+
+import android.os.Handler
+import android.os.HandlerThread
+import android.os.Looper
+
+/**
+ * Abstraction of our interactions with handlers/threads for isolation purposes
+ */
+interface ThreadHelper {
+    fun getHandler(looper: Looper): Handler
+    fun getHandlerThread(name: String?): HandlerThread
+    fun runOnUiThread(block: () -> Unit)
+}
