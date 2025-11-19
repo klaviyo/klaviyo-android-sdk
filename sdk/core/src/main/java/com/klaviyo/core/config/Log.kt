2f0c4c710b119e0bcc002cdbfa0ca7ec34db6adc
Evan Masseau <>
2025-11-18 21:13:10 -0500

    Added support for writing SDK logs to a file and viewing, downloading or sharing those log files.
    Developers who want to use the file logger's sharing capabilities (openLogInViewer,
    shareLogs) now need to add the FileProvider declaration and XML configuration to
    their own manifest, following the instructions in the FileLogger KDoc.

--- sdk/core/src/main/java/com/klaviyo/core/config/Log.kt
+++ sdk/core/src/main/java/com/klaviyo/core/config/Log.kt
@@ -1,5 +1,7 @@
 package com.klaviyo.core.config
 
+import com.klaviyo.core.utils.AdvancedAPI
+
 interface Log {
 
     /**
@@ -16,7 +18,7 @@ interface Log {
     }
 
     /**
-     * The minimum log level to output
+     * The minimum log level to write to system logs
      *
      * To configure from manifest, specify the minimum log level to output:
      *
@@ -29,6 +31,22 @@ interface Log {
      * 6 = Assert only
      */
     var logLevel: Level
+
+    /**
+     * Register a log interceptor to be notified of all log calls (regardless of level).
+     *
+     * @param interceptor The interceptor to be invoked for every log event
+     */
+    @AdvancedAPI
+    fun addInterceptor(interceptor: LogInterceptor)
+
+    /**
+     * Unregister a previously registered log interceptor
+     *
+     * @param interceptor The interceptor to remove
+     */
+    @AdvancedAPI
+    fun removeInterceptor(interceptor: LogInterceptor)
 
     /**
      * Verbose output
@@ -78,3 +96,16 @@ interface Log {
      */
     fun wtf(message: String, ex: Throwable? = null)
 }
+
+/**
+ * Interceptor for log events. Allows custom logging hooks to be built on top of the default behavior
+ *
+ * Example usage:
+ * ```
+ * KLog.addInterceptor { level, tag, message, throwable ->
+ *     // Log to file, send to analytics, etc.
+ *     // Filter by level, if desired
+ * }
+ * ```
+ */
+typealias LogInterceptor = (level: Log.Level, tag: String, message: String, throwable: Throwable?) -> Unit
